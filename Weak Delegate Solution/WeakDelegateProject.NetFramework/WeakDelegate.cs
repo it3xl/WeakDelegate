59fe2f3615b6276fbce0aa3ae4d315383d8ef639
Ilya Tretyakov <it@it3xl.com>
2013-12-29 22:03:13 +0400

    Initial commit.

---
+++ Weak Delegate Solution/WeakDelegateProject.NetFramework/WeakDelegate.cs
@@ -0,0 +1,17 @@
+﻿namespace It3xl.WeakDelegateProject
+{
+	using System;
+
+	/// <summary>
+	/// TODO.it3xl.com: To write the comment.
+	/// </summary>
+    public class WeakDelegate
+    {
+		public WeakDelegate(Delegate inputDelegate)
+		{
+			// Посмотреть, какой будет Invocation list у вложенный перевложенных делегатов.
+
+			var inv = inputDelegate.GetInvocationList();
+		}
+    }
+}
